e59a6368cda407ec67e04c8c4ca070340883bfd0
Serkan Sozer <eserkansozer@hotmail.com>
2020-05-28 15:02:13 +0100

    SAD v1

--- API/SFB.Web.Api/Controllers/SelfAssesmentController.cs
+++ API/SFB.Web.Api/Controllers/SelfAssesmentController.cs
@@ -5,6 +5,7 @@ using SFB.Web.ApplicationCore.Helpers.Enums;
 using SFB.Web.ApplicationCore.Models;
 using SFB.Web.ApplicationCore.Services.DataAccess;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace SFB.Web.Api.Controllers
@@ -37,12 +38,10 @@ namespace SFB.Web.Api.Controllers
 
             try
             {
-                var schoolContextData = await _contextDataService.GetSchoolDataObjectByUrnAsync(urn);
+                var schoolContextData = await _contextDataService.GetSchoolDataObjectByUrnAsync(urn);//TODO: Do we need all the context data?
                 var financeType = (EstablishmentType)Enum.Parse(typeof(EstablishmentType), schoolContextData.FinanceType);
-                var schoolFinancialData = await _financialDataService.GetSchoolFinancialDataObjectAsync(urn, financeType, CentralFinancingType.Include);
-                var term = await _financialDataService.GetLatestDataYearPerEstabTypeAsync(financeType);
 
-                selfAssesmentModel = await BuildSelfAssesmentModel(urn, financeType, schoolContextData, schoolFinancialData, term);
+                selfAssesmentModel = await BuildSelfAssesmentModel(urn, financeType);
             }
             catch (Exception ex)
             {
@@ -52,17 +51,32 @@ namespace SFB.Web.Api.Controllers
             return selfAssesmentModel;
         }
 
-        private async Task<SelfAssesmentModel> BuildSelfAssesmentModel(int urn, EstablishmentType financeType, EdubaseDataObject schoolContextData, SchoolTrustFinancialDataObject schoolFinancialData, int term)
+        private async Task<SelfAssesmentModel> BuildSelfAssesmentModel(int urn, EstablishmentType financeType)
         {
-            var termYears = $"{term - 1}/{term}";
-            var model = new SelfAssesmentModel();
-            model.Urn = urn;
-            model.Name = schoolContextData.EstablishmentName;
-            model.FinancialData = schoolFinancialData;
+            var schoolFinancialData = await _financialDataService.GetSchoolFinancialDataObjectAsync(urn, financeType, CentralFinancingType.Include);
+            var model = new SelfAssesmentModel(urn, schoolFinancialData.SchoolName, schoolFinancialData.OverallPhase, bool.Parse(schoolFinancialData.Has6Form));
+            string termYears = await GetTermYears(financeType);
             model.SadSizeLookup = await _selfAssesmentDashboardDataService.GetSADSizeLookupDataObject(schoolFinancialData.OverallPhase, bool.Parse(schoolFinancialData.Has6Form), schoolFinancialData.NoPupils.GetValueOrDefault(), termYears);
             model.SadFSMLookup = await _selfAssesmentDashboardDataService.GetSADFSMLookupDataObject(schoolFinancialData.OverallPhase, bool.Parse(schoolFinancialData.Has6Form), schoolFinancialData.PercentageFSM.GetValueOrDefault(), termYears);
-            model.RatingForTeachingStaff = await _selfAssesmentDashboardDataService.GetSADSchoolRatingsDataObjectAsync("Teaching staff", financeType, schoolFinancialData.OverallPhase, bool.Parse(schoolFinancialData.Has6Form), schoolFinancialData.LondonWeight, model.SadSizeLookup.SizeType, model.SadFSMLookup.FSMScale, schoolFinancialData.TeachingStaff.GetValueOrDefault() / schoolFinancialData.TotalExpenditure.GetValueOrDefault(), termYears);
+
+            model.SadAssesmentAreas = new List<SadAssesmentAreaModel>();
+
+            await AddAssessmentArea("Teaching staff", financeType, schoolFinancialData.TeachingStaff.GetValueOrDefault(), schoolFinancialData.TotalExpenditure.GetValueOrDefault(), schoolFinancialData, model, termYears);
+
             return model;
+        }
+
+        private async Task AddAssessmentArea(string areaName, EstablishmentType financeType, decimal schoolData, decimal total, SchoolTrustFinancialDataObject schoolFinancialData, SelfAssesmentModel model, string termYears)
+        {
+            List<SADSchoolRatingsDataObject> teachingStaffTresholds = await _selfAssesmentDashboardDataService.GetSADSchoolRatingsDataObjectAsync(areaName, financeType, schoolFinancialData.OverallPhase, bool.Parse(schoolFinancialData.Has6Form), schoolFinancialData.LondonWeight, model.SadSizeLookup.SizeType, model.SadFSMLookup.FSMScale, schoolData / total, termYears);
+            model.SadAssesmentAreas.Add(new SadAssesmentAreaModel(areaName, schoolData, schoolData / total, teachingStaffTresholds));
+        }
+
+        private async Task<string> GetTermYears(EstablishmentType financeType)
+        {
+            var term = await _financialDataService.GetLatestDataYearPerEstabTypeAsync(financeType);
+            var termYears = $"{term - 1}/{term}";
+            return termYears;
         }
     }
 }
